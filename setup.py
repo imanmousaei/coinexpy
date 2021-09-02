4fa33fd4d009928f5e9af279ef099886c7f0f310
Iman Mousaei <imanmousaei@sotoon.ir>
2021-09-02 21:09:06 +0430

    Add description for pypi

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from distutils.core import setup
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='coinexpy',
     packages=['coinexpy'],
     version='0.1',
     license='MIT',
     description='Python wrapper for Coinex APIs',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author='Iman Mousaei',
     author_email='imanmousaei1379@gmail.com',
     url='https://github.com/imanmousaei/coinexpy',
